d5a9eb8890f667d5a74230b2690df0fcadace380
Lucrece DES COURTIS <ldes-cou@e3r12p1.42.fr>
2020-11-19 10:48:23 +0100

    ft_strrchr

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: ldes-cou <ldes-cou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/19 09:29:55 by ldes-cou          #+#    #+#             */
-/*   Updated: 2020/11/19 10:18:09 by ldes-cou         ###   ########.fr       */
+/*   Updated: 2020/11/19 10:47:17 by ldes-cou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,11 @@ char *strchr(const char *s, int c)
 	while(*s)
 	{
 		if(s[i] == c)
-			return((char *)s[i]);
+			return((char *)s + i);
 		i++;
 
 	}
 	if(s[i] == c)
-		return((char*)s[i]);
+		return((char*)s + i);
 	return(NULL);
 }
--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,10 +6,25 @@
 /*   By: ldes-cou <ldes-cou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/19 09:43:40 by ldes-cou          #+#    #+#             */
-/*   Updated: 2020/11/19 10:03:57 by ldes-cou         ###   ########.fr       */
+/*   Updated: 2020/11/19 10:46:23 by ldes-cou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char * strrchr(const char *s, int c)
+#include <libft.h>
+
+char *strrchr(const char *s, int c)
 {
+	int i;
 	
+	i = ft_strlen(s);
+	while(*s)
+	{
+		if(s[i] == c);
+			return((char *)s + i);
+		i++;
+	}
+	if(s[i] == c)
+		return((char *)s + i);
+	return(NULL);
+}
+				
