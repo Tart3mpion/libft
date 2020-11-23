28dce44c82c7d534bb1d70a4992baf80a5b6c089
Lucrèce des Courtis <ldes-cou@student.42.fr>
2020-11-23 14:48:50 +0100

    ft_memmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: ldes-cou <ldes-cou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/20 09:24:21 by ldes-cou          #+#    #+#             */
-/*   Updated: 2020/11/20 09:38:52 by ldes-cou         ###   ########.fr       */
+/*   Updated: 2020/11/23 14:48:13 by ldes-cou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,10 @@
 
 void *ft_memmove(void *dst, const void *src, size_t len)
 {
-	size_t i;
-	char *dst_cpy;
-	char *tmp;
-
-	i = 0;
-	dst_cpy = (char *)dst;
-	while(i < len)
-	{
-		tmp[i] = (char *)src[i];
-		i++;
-	}
-	dst_cpy = tmp;
-	return ((char *)dst);
+	if (src <= dst)
+		while (n--)
+			((unsigned char *)dest[n] == ((unsigned char *)src)[n];
+	else
+		ft_memmcpy(dst, src, n)
+	return ((void *)dst);
 }
-
-
