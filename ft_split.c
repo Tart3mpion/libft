6465183370daabc77508798c526dc70d77c3b229
ldes-cou <ldes-cou@student.42.fr>
2020-11-28 09:45:50 +0100

    split

--- ft_split.c
+++ ft_split.c
@@ -6,11 +6,58 @@
 /*   By: ldes-cou <ldes-cou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/24 11:42:41 by ldes-cou          #+#    #+#             */
-/*   Updated: 2020/11/24 11:57:28 by ldes-cou         ###   ########.fr       */
+/*   Updated: 2020/11/28 09:44:58 by ldes-cou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char **ft_split(char const *s, char c)
-{
-	char **str;
+#include "libft.h"
 
+static int ft_len_word(char *s, int start, char c)
+{
+	int i;
+
+	i = 0;
+	while (*s)
+	{
+		while (*s == c)
+			s++;
+		while (*s != c)
+			i++;
+	}
+	return (i);
+}
+
+static 	int ft_num_word(char *s, char c)
+{
+	int i;
+	
+	i = 0;
+	while (*s)
+	{
+		while (*s != c)
+			s++;
+		i++;
+		while (*s == c)
+			S++;
+	}
+	return (i);
+}
+
+char **		ft_split(char *s, char c)
+{
+	int start;
+	char **array;
+	int i;
+	int nb_word;
+	int len;
+	
+	i = 0;
+	start = 0;
+	nb_word = ft_num_word(*s, c);
+	if (!(array = (char *)malloc(sizeof(char **) * (nb_word +1))));
+		return (NULL);
+	while (*s && i < nb_word)
+	{
+			
+			len = ft_len_word(*s, start, c);
+			*array[i] = ft_substr(*s, start, len)
--- libft.h
+++ libft.h
@@ -34,4 +34,5 @@ char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *s2);
 char	*itoa(int n);
+char	**ft_split(char const *s, char c);
 #endif
